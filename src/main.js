fb055ae39839af88828ed5289df5054ddc439e12
KarlaDelAngel8 <karladelangelsantiago03@gmail.com>
2025-04-30 12:23:44 -0600

    implementacion/main layoud

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
+import './assets/scss/white-dashboard.scss'
+import  './assets/css/nucleo-icons.css'
 const app = createApp(App)
-
-app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
